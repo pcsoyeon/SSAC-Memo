8e2463e537ddd8d23c35932e47acc104c3ed2432
so yeon <alicesysy@swu.ac.kr>
2022-09-01 07:40:55 +0900

    [#9] Dark/Light Mode Color Asset, Extension 추가

---
+++ Memo/Memo/Global/Extension/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  Memo
+//
+//  Created by 소연 on 2022/09/01.
+//
+
+import UIKit
+
+extension UIColor {
+    static let background = UIColor(named: "Background")
+    static let foreground = UIColor(named: "Foreground")
+    static let text = UIColor(named: "Text")
+}
