40fa85093418a58b38c81c701d4094102824fdca
so yeon <alicesysy@swu.ac.kr>
2022-09-02 15:52:57 +0900

    [#5] Realm Database Memo 모델 추가

---
+++ Memo/Memo/Global/Realm/Memo.swift
@@ -0,0 +1,27 @@
+//
+//  Memo.swift
+//  Memo
+//
+//  Created by 소연 on 2022/09/02.
+//
+
+import Foundation
+
+import RealmSwift
+
+class Memo: Object {
+    @Persisted var memoTitle: String
+    @Persisted var memoContent: String?
+    @Persisted var memoDate = Date()
+    @Persisted var isPinned: Bool
+    
+    @Persisted(primaryKey: true) var objectId: ObjectId
+    
+    convenience init(memoTitle: String, memoContent: String?, memoDate: Date, isPinned: Bool = false) {
+        self.init()
+        self.memoTitle = memoTitle
+        self.memoContent = memoContent
+        self.memoDate = memoDate
+        self.isPinned = isPinned
+    }
+}
