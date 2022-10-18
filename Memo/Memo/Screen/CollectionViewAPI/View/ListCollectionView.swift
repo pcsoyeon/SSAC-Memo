af82ef9599da08bfd0ef4b0b26ff26d06511681f
so yeon <alicesysy@swu.ac.kr>
2022-10-18 16:36:03 +0900

    [#27] ListConfiguration으로 메모 리스트 UI 구현

---
+++ Memo/Memo/Screen/CollectionViewAPI/View/ListCollectionView.swift
@@ -0,0 +1,34 @@
+//
+//  ListCollectionView.swift
+//  Memo
+//
+//  Created by 소연 on 2022/10/18.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ListCollectionView: BaseView {
+    
+    // MARK: - UI Property
+    
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    
+    let configuration = UICollectionLayoutListConfiguration(appearance: .grouped)
+    private lazy var layout = UICollectionViewCompositionalLayout.list(using: configuration)
+    
+    // MARK: - UI Method
+    
+    override func configureUI() {
+        backgroundColor = .systemBackground
+    }
+    
+    override func setConstraints() {
+        addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalTo(self.safeAreaLayoutGuide)
+        }
+    }
+}
